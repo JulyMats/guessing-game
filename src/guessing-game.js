df64838e0829ec7ffb7d07e5f9d975993161778a
Matsukevich Yuliya <july.matsukevich@mail.ru>
2023-10-12 16:16:49 +0300

    add solution to the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 1;
+        this.max = 1;
+        this.guessNum = 1;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNum = Math.ceil((this.min + this.max) / 2);
+        return this.guessNum;
     }
 
     lower() {
-
+        this.max = this.guessNum;
     }
 
     greater() {
-
+        this.min = this.guessNum;
     }
 }
 
